b02f6e98ec5065b4592a608d9f6b45dc48a791a1
abhyuday-fr <abhyudaypundir07@gmail.com>
2026-03-27 22:35:14 +0530

    added multiple requests (non concurrent)

--- client.cpp
+++ client.cpp
@@ -1,16 +1,95 @@
 #include <iostream>
 #include <cstdlib>
-#include <string>
+#include <cstdint>
+#include <cstring>
 #include <cerrno>
+#include <cassert>
+#include <string_view>
+
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-static void die(const std::string& msg){
+static void msg(const std::string_view& msg){
+    std::cerr << msg << '\n';
+}
+
+static void die(const std::string_view& msg){
     int err = errno;
     std::cerr << "[" << err << "] " << msg << '\n';
     std::abort();
+}
+
+static int32_t read_full(int fd, char *buf, size_t n){
+    while(n > 0){
+        ssize_t rv = ::read(fd, buf, n);
+        if(rv <= 0){
+            return -1; // error, EOF
+        }
+        assert(static_cast<size_t>(rv) <= n);
+        n -= static_cast<size_t>(rv);
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n){
+    while(n > 0){
+        ssize_t rv = ::write(fd, buf, n);
+        if(rv <= 0){
+            return -1; // error
+        }
+        assert(static_cast<size_t>(rv) <= n);
+        n -= static_cast<size_t>(rv);
+        buf += rv;
+
+    }
+    return 0;
+}
+
+constexpr size_t k_max_msg = 4096;
+
+static int32_t query(int fd, std::string_view text){
+    uint32_t len = static_cast<uint32_t>(text.length());
+    if(len > k_max_msg){
+        return -1;
+    }
+
+    char wbuf[4 + k_max_msg];
+    std::memcpy(wbuf, &len, 4); // assume little endian
+    std::memcpy(&wbuf[4], text.data(), len);
+    if(int32_t err = write_all(fd, wbuf, 4 + len)){
+        return err;
+    }
+
+    // 4 bytes header
+    char rbuf[4 + k_max_msg];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if(err){
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+
+    std::memcpy(&len, rbuf, 4); // assume little endian
+    if(len > k_max_msg){
+        msg("too long");
+        return -1;
+    }
+
+    // reply body
+    err = read_full(fd, &rbuf[4], len);
+    if(err){
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    std::cout << "server says: " << std::string_view(&rbuf[4], len) << '\n';
+
+    return 0;
+
 }
 
 int main(){
@@ -29,18 +108,24 @@ int main(){
         die("connect");
     }
 
-    std::string msg = "hello";
-    ::write(fd, msg.data(), msg.length());
-
-    char rbuf[64] = {};
-    ssize_t n = ::read(fd, rbuf, sizeof(rbuf) - 1);
-    if(n < 0){
-        die("read");
+    // multiple requests
+    int32_t err = query(fd, "hello1");
+    if(err){
+        goto L_DONE;
     }
 
-    std::cout << "server says: " << rbuf << '\n';
+    err = query(fd, "hello2");
+    if(err){
+        goto L_DONE;
+    }
 
-    ::close(fd);
+    err = query(fd, "hello3");
+    if(err){
+        goto L_DONE;
+    }
 
+    L_DONE:
+        ::close(fd);
+    
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,34 +1,92 @@
 #include <iostream>
 #include <cstdlib>
-#include <string>
+#include <cstdint>
+#include <cstring>
 #include <cerrno>
+#include <cassert>
+#include <string_view>
+
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-static void msg(const std::string& msg) {
+static void msg(const std::string_view& msg) {
     std::cerr << msg << '\n';
 }
 
-static void die(const std::string& msg) {
+static void die(const std::string_view& msg) {
     int err = errno;
     std::cerr << "[" << err << "] " << msg << '\n';
     std::abort();
 }
 
-static void do_something(int connfd) {
-    char rbuf[64] = {};
-    ssize_t n = ::read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-        msg("read() error");
-        return;
+constexpr size_t k_max_msg = 4096;
+
+static int32_t read_full(int fd, char *buf, size_t n){
+    while(n > 0){
+        ssize_t rv = ::read(fd, buf, n);
+        if(rv <= 0){
+            return -1;
+        }
+        assert(static_cast<size_t>(rv) <= n);
+        n -= static_cast<size_t>(rv);
+        buf += rv;
     }
-    
-    std::cerr << "client says: " << rbuf << '\n';
-    
-    std::string wbuf = "world";
-    ::write(connfd, wbuf.data(), wbuf.length());
+    return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n){
+    while(n > 0){
+        ssize_t rv = ::write(fd, buf, n);
+        if(rv <= 0){
+            return -1;
+        }
+        assert(static_cast<size_t>(rv) <= n);
+        n -= static_cast<size_t>(rv);
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t one_request(int connfd){
+    // 4 bytes header
+    char rbuf[4 + k_max_msg];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if(err){
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+
+    uint32_t len = 0;
+    std::memcpy(&len, rbuf, 4); // assume little endian
+    if(len > k_max_msg){
+        msg("too long");
+        return -1;
+    }
+
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if(err){
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    std::cerr << "client says: " << std::string_view(&rbuf[4], len) << '\n';
+
+    // reply using the same protocol
+    constexpr std::string_view reply = "world";
+    constexpr size_t reply_len = reply.length();
+
+    char wbuf[4 + reply_len];
+
+    len = static_cast<uint32_t>(reply_len);
+    std::memcpy(wbuf, &len, 4);
+    std::memcpy(&wbuf[4], reply.data(), len);
+
+    return write_all(connfd, wbuf, 4 + len);
 }
 
 int main(){
@@ -71,7 +129,13 @@ int main(){
             continue; //error
         }
 
-        do_something(connfd);
+        while(true){
+            // here the server serves only one client connection once
+            int32_t err = one_request(connfd);
+            if(err){
+                break;
+            }
+        }
         ::close(connfd);
     }
 
