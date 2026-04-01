1933654734e29e063b893b7629c4f8065a5d4f5e
abhyuday-fr <abhyudaypundir07@gmail.com>
2026-04-01 23:35:01 +0530

    implemented event-based concurrency using polling

--- client.cpp
+++ client.cpp
@@ -4,6 +4,8 @@
 #include <cstring>
 #include <cerrno>
 #include <cassert>
+#include <string>
+#include <vector>
 #include <string_view>
 
 #include <unistd.h>
@@ -21,7 +23,7 @@ static void die(const std::string_view& msg){
     std::abort();
 }
 
-static int32_t read_full(int fd, char *buf, size_t n){
+static int32_t read_full(int fd, uint8_t *buf, size_t n){
     while(n > 0){
         ssize_t rv = ::read(fd, buf, n);
         if(rv <= 0){
@@ -34,7 +36,7 @@ static int32_t read_full(int fd, char *buf, size_t n){
     return 0;
 }
 
-static int32_t write_all(int fd, const char *buf, size_t n){
+static int32_t write_all(int fd, const uint8_t *buf, size_t n){
     while(n > 0){
         ssize_t rv = ::write(fd, buf, n);
         if(rv <= 0){
@@ -48,48 +50,67 @@ static int32_t write_all(int fd, const char *buf, size_t n){
     return 0;
 }
 
-constexpr size_t k_max_msg = 4096;
+// append to the back
+static void buf_append(std::vector<uint8_t> &buf, const uint8_t *data, size_t len){
+    buf.insert(buf.end(), data, data + len);
+}
 
-static int32_t query(int fd, std::string_view text){
-    uint32_t len = static_cast<uint32_t>(text.length());
-    if(len > k_max_msg){
+constexpr size_t k_max_msg = 32 << 20; //likely larger than the kernel buffer
+
+//the 'query' function (see previous commit to see that) simply split into  'send_req' and 'read_res'
+
+static int32_t send_req(int fd, const uint8_t *text, size_t len){
+    uint32_t msg_len = static_cast<uint32_t>(len);
+    if(msg_len > k_max_msg){
         return -1;
     }
 
-    char wbuf[4 + k_max_msg];
-    std::memcpy(wbuf, &len, 4); // assume little endian
-    std::memcpy(&wbuf[4], text.data(), len);
-    if(int32_t err = write_all(fd, wbuf, 4 + len)){
-        return err;
-    }
+    std::vector<uint8_t> wbuf;
+    //explicitly copy from a 4 byte uint32_t to avoid size_t architecture mismatches
+    buf_append(wbuf, reinterpret_cast<const uint8_t*>(&msg_len), 4);
+    buf_append(wbuf, text, len);
 
+    return write_all(fd, wbuf.data(), wbuf.size());
+}
+
+static int32_t read_res(int fd){
     // 4 bytes header
-    char rbuf[4 + k_max_msg];
+    std::vector<uint8_t> rbuf;
+    rbuf.resize(4);
     errno = 0;
-    int32_t err = read_full(fd, rbuf, 4);
+    int32_t err = read_full(fd, rbuf.data(), 4);
     if(err){
-        msg(errno == 0 ? "EOF" : "read() error");
+        if(errno == 0){
+            msg("EOF");
+        }
+        else{
+            msg("read() error");
+        }
         return err;
     }
 
-    std::memcpy(&len, rbuf, 4); // assume little endian
+    uint32_t len = 0;
+    std::memcpy(&len, rbuf.data(), 4); // assume little endian
     if(len > k_max_msg){
         msg("too long");
         return -1;
     }
 
     // reply body
+    rbuf.resize(4 + len);
     err = read_full(fd, &rbuf[4], len);
     if(err){
         msg("read() error");
         return err;
     }
 
-    // do something
-    std::cout << "server says: " << std::string_view(&rbuf[4], len) << '\n';
+    // safely truncate to 100 characters for printing using string_view
+    size_t print_len = len < 100 ? len : 100;
+    std::string_view reply_text(reinterpret_cast<const char*>(&rbuf[4]), print_len);
+
+    std::cout << "len: " << len << " data:" << reply_text << '\n';
 
     return 0;
-
 }
 
 int main(){
@@ -108,20 +129,26 @@ int main(){
         die("connect");
     }
 
-    // multiple requests
-    int32_t err = query(fd, "hello1");
-    if(err){
-        goto L_DONE;
+    // multiple pipelined requests
+    std::vector<std::string> query_list = {
+        "hello1", "hello2", "hello3",
+        // a large message requires multiple event loop iterations
+        std::string(k_max_msg, 'z'),
+        "hello5",
+    };
+
+    for(const std::string &s : query_list){
+        int32_t err = send_req(fd, reinterpret_cast<const uint8_t*>(s.data()), s.size());
+        if(err){
+            goto L_DONE;
+        }
     }
 
-    err = query(fd, "hello2");
-    if(err){
-        goto L_DONE;
-    }
-
-    err = query(fd, "hello3");
-    if(err){
-        goto L_DONE;
+    for(size_t i = 0; i < query_list.size(); i++){
+        int32_t err = read_res(fd);
+        if(err){
+            goto L_DONE;
+        }
     }
 
     L_DONE:
